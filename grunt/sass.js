3d31e5eb0aaff8bd2796e227dc873379314adb0d
David Maran <david@dmaran.com>
2016-03-27 12:31:31 +0100

    add foundation path

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,7 +1,8 @@
 module.exports = {
   dist: {                            // Target
       options: {                       // Target options
-        style: 'expanded'
+        style: 'expanded',
+        loadPath: ['node_modules/foundation/scss']
       },
       files: {                         // Dictionary of files
         'dev/assets/css/style.css': 'raw/scss/style.scss'
