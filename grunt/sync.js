70b2445adfb1640553f1ed9a44ade693e0348b7e
David Maran <david@dmaran.com>
2016-03-28 19:59:50 +0100

    sync files
    sync files across to wordpress

--- grunt/sync.js
+++ grunt/sync.js
@@ -1,7 +1,9 @@
 module.exports = {
   main: {
     files: [
-
+    	{cwd: 'dist/assets/js', src: ['**'], dest: 'wordpress/theme/figit-theme/assets/js/'},
+    	{cwd: 'dist/assets/css', src: ['**'], dest: 'wordpress/theme/figit-theme/assets/css/'},
+    	{cwd: 'dist/assets/img', src: ['**'], dest: 'wordpress/theme/figit-theme/assets/images/'},
     ]
 }
 };
