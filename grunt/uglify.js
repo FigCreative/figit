e060c0ce76de8204c89f48e7e88714e77f26b362
David Maran <david@dmaran.com>
2016-03-28 20:02:37 +0100

    correct .min files

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,7 +1,7 @@
 module.exports = {
   dist: {
     files: {
-      'dist/assets/js/build.min.js': ['dev/assets/js/build.js']
+      'dist/assets/js/app.min.js': ['dev/assets/js/app.js']
     }
   }
 };
